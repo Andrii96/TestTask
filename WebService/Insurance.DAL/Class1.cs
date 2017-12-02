2ebec6fdba21a6e99d14fe6d1178ab467aa5e31b
Andrew Popilevych <apopilevych.coi@gmail.com>
2017-12-02 14:31:31 +0200

    Project structure change

--- WebService/Insurance.DAL/Class1.cs
+++ WebService/Insurance.DAL/Class1.cs
@@ -8,5 +8,6 @@ namespace Insurance.DAL
 {
     public class Class1
     {
+
     }
 }
---
+++ WebService/Insurance.DAL/Entities/SystemA/Insurer.cs
@@ -0,0 +1,20 @@
+﻿using Insurance.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance.DAL.Entities.System1
+{
+    public class Insurer:Entity
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
