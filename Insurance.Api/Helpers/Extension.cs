d55d563ec72e2e4a33d7c4aa13a53fde4afedaf8
Andrew Popilevych <apopilevych.coi@gmail.com>
2017-12-06 15:55:17 +0200

    Merge methods update

--- Insurance.Api/Helpers/Extension.cs
+++ Insurance.Api/Helpers/Extension.cs
@@ -6,64 +6,64 @@ using System.Web;
 
 namespace Insurance.Api.Helpers
 {
-    //public static class Extension
-    //{
-    //    public static T MergeObjects<T>(this IEnumerable<T> list) where T : new()
-    //    {
-    //        var resultObject = new T();
-    //        var properties = typeof(T).GetProperties();
+    public static class Extension
+    {
+        public static T MergeObjectsDynamicly<T>(this IEnumerable<T> list) where T : new()
+        {
+            var resultObject = new T();
+            var properties = typeof(T).GetProperties();
 
-    //        foreach (var item in list)
-    //        {
-    //            if (item == null) continue;
+            foreach (var item in list)
+            {
+                if (item == null) continue;
 
-    //            foreach (var property in properties)
-    //            {
-    //                var value = property.GetValue(item);
-    //                var resultObjectPropertyValue = property.GetValue(resultObject);
+                foreach (var property in properties)
+                {
+                    var value = property.GetValue(item);
+                    var resultObjectPropertyValue = property.GetValue(resultObject);
 
-    //                if (value!=null && !value.Equals(DefaultValue(property.PropertyType)))
-    //                {
-    //                    if (property.PropertyType.DoesTypeSupportInterface(typeof(IEnumerable)) && resultObjectPropertyValue != null)
-    //                    {
-    //                        var unionMethod = typeof(Enumerable).GetMethods()
-    //                                                            .Single(m => m.Name == "Union" &&
-    //                                                                        m.GetGenericArguments().Length == 1 &&
-    //                                                                        m.GetParameters().Length == 2)
-    //                                                            .MakeGenericMethod(property.PropertyType
-    //                                                                                        .GetGenericArguments()
-    //                                                                                        .First());
+                    if (value != null && !value.Equals(DefaultValue(property.PropertyType)))
+                    {
+                        if (property.PropertyType.DoesTypeSupportInterface(typeof(IEnumerable)) && resultObjectPropertyValue != null)
+                        {
+                            var unionMethod = typeof(Enumerable).GetMethods()
+                                                                .Single(m => m.Name == "Union" &&
+                                                                            m.GetGenericArguments().Length == 1 &&
+                                                                            m.GetParameters().Length == 2)
+                                                                .MakeGenericMethod(property.PropertyType
+                                                                                            .GetGenericArguments()
+                                                                                            .First());
 
-    //                        var union = unionMethod.Invoke(null, new object[] { resultObjectPropertyValue, value });
+                            var union = unionMethod.Invoke(null, new object[] { resultObjectPropertyValue, value });
 
-    //                        var toListMethod = typeof(Enumerable).GetMethod("ToList")
-    //                                                                         .MakeGenericMethod(property.PropertyType
-    //                                                                                                    .GetGenericArguments()
-    //                                                                                                    .First());
-    //                        value = toListMethod.Invoke(null, new object[] { union });
+                            var toListMethod = typeof(Enumerable).GetMethod("ToList")
+                                                                             .MakeGenericMethod(property.PropertyType
+                                                                                                        .GetGenericArguments()
+                                                                                                        .First());
+                            value = toListMethod.Invoke(null, new object[] { union });
 
-    //                    }
+                        }
 
-    //                    property.SetValue(resultObject, value);
-    //                }
-    //            }
-    //        }
-    //        return resultObject;
-    //    }
+                        property.SetValue(resultObject, value);
+                    }
+                }
+            }
+            return resultObject;
+        }
 
-    //    public static bool DoesTypeSupportInterface(this Type type, Type interfaceType)
-    //    {
-    //        return type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
-    //    }
+        public static bool DoesTypeSupportInterface(this Type type, Type interfaceType)
+        {
+            return type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
+        }
 
-    //    private static object DefaultValue(Type T)
-    //    {
-    //        if (T.IsValueType)
-    //        {
-    //            var instance = Activator.CreateInstance(T);
-    //            return instance;
-    //        }
-    //        return null;
-    //    }
-    //}
+        private static object DefaultValue(Type T)
+        {
+            if (T.IsValueType)
+            {
+                var instance = Activator.CreateInstance(T);
+                return instance;
+            }
+            return null;
+        }
+    }
 }
